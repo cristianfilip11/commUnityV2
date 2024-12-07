3c2c911fca45be98b8e658646c878ceff7f03245
cristianfilip11 <filipcristian32@gmail.com>
2024-12-07 16:15:00 +0200

    Initial commit

---
+++ src/test/java/com/example/commUnity/CommUnityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commUnity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommUnityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
